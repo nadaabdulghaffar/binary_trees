6ba8dc6360eeff3130ba988d4d9ea96daa9880fc
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-06 19:06:02 +0300

    task balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
